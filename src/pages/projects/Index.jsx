4036bdf93d4e992634b22a31b4c408b55040e746
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-08-19 21:52:08 -0300

    adicionando as paginas de projetos

--- src/pages/projects/Index.jsx
+++ src/pages/projects/Index.jsx
@@ -4,14 +4,17 @@ import CardProject from '../../shared/layout/CardProject';
 
 const Index = () => {
     const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchProjects = async () => {
             try {
                 const data = await projectsApi.getProjectPosts();
                 setProjects(data);
+                setLoading(false);
             } catch (err) {
                 console.log('Error fetching projects: ', err);
+                setLoading(false);
             }
         };
 
@@ -21,19 +24,21 @@ const Index = () => {
     return (
         <div className="container mx-auto py-10">
             <h1 className="text-4xl font-semibold mb-6">Projects</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                {projects.map(project => (
-                    <CardProject
-                        key={project.id} // Make sure to add the key
-                        title={project.title}
-                        description={project.description}
-                        src={project.src}
-                        link={`/projects/${project.id}`}
-                    />
-
-
-                ))}
-            </div>
+            {loading ? (
+                <p>Loading projects...</p>
+            ) : (
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                    {projects.map(project => (
+                        <CardProject
+                            key={project.id}
+                            title={project.title}
+                            description={project.description}
+                            src={project.src}
+                            link={`/projects/${project.id}`}
+                        />
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
