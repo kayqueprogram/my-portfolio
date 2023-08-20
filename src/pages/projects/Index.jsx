118a0c3ed98a47aecb3a037b26f9c690d41feef3
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-08-19 21:34:54 -0300

    adicionando as paginas de projetos

--- src/pages/projects/Index.jsx
+++ src/pages/projects/Index.jsx
@@ -24,12 +24,14 @@ const Index = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {projects.map(project => (
                     <CardProject
-                        key={project.id}
+                        key={project.id} // Make sure to add the key
                         title={project.title}
                         description={project.description}
                         src={project.src}
-                        link={project.link}
+                        link={`/projects/${project.id}`}
                     />
+
+
                 ))}
             </div>
         </div>
