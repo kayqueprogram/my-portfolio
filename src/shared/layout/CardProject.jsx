ad4016cf69149bddda5d320a5d4e94879078c360
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-10-08 11:54:42 -0300

    language

--- src/shared/layout/CardProject.jsx
+++ src/shared/layout/CardProject.jsx
@@ -19,7 +19,7 @@ export default function CardProject({ title, description, src, link }) {
           rel="noopener noreferrer"
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          View Page
+          Ver página
           <svg
             className="w-4 h-4 ml-2"
             aria-hidden="true"
